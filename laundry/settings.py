f73fd49c4e9f0620717355f31d141ba32a862421
andika262 <pangestu.andika46@gmail.com>
2023-01-06 22:43:38 +0700

    deployyy

--- laundry/settings.py
+++ laundry/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-q#_(#ha$3(w0$@70tz&9o902)58!(tc2*i7@u#2vvf3-_2rso!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "web-production-7385.up.railway.app"]
 
 
 # Application definition
